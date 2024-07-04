e2263c37a24df13bd7d6e64e4ce9b4d8ba994a2d
AndreeaSamoila <a.samoilaa@yahoo.com>
2024-07-04 10:41:38 +0300

    AP-3 tables changes

--- database/seed/seed.sql
+++ database/seed/seed.sql
@@ -1,18 +1,19 @@
 CREATE TABLE 'client' (
     'clientId' int(10) NOT NULL AUTO_INCREMENT,
-    'client_last_name' varchar(128) NOT NULL,
-    'client_first_name' varchar(128) NOT NULL,
+    'last_name' varchar(128) NOT NULL,
+    'first_name' varchar(128) NOT NULL,
     'email' varchar(25) NOT NULL,
     'phone_number' varchar(10) NOT NULL,
     PRIMARY KEY ('clientId'),
+    UNIQUE KEY `IDX_CLIENT_EMAIL` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 
 CREATE TABLE 'room' (
     'roomId' int(100) NOT NULL AUTO_INCREMENT,
-    'description' varchar(200) NOT NULL, 
-    'bed_type' enum ('Single Bed', 'Double Bed', 'King Bed', 'Twin Bed') NOT NULL
+    'description' LONGTEXT NOT NULL, 
+    'bed_type' enum ('single_bed', 'double_bed', 'king_bed', 'twin_bed') NOT NULL
     'adults_number' int(3) NOT NULL,
-    'children_number' int(3) NOT NULL,
+    'children_number' int(3)  DEFAULT 0,
     'is_reserved' tinyint(4) NOT NULL DEFAULT 0,
     'price_per_night' int(5) NOT NULL,
  PRIMARY KEY ('roomId'),
