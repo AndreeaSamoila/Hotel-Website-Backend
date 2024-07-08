d82c8a9c22fa782e96f5faca00091cb483f477ae
AndreeaSamoila <a.samoilaa@yahoo.com>
2024-07-08 17:32:19 +0300

    AP-3 small changes

--- database/seed/seed.sql
+++ database/seed/seed.sql
@@ -11,22 +11,22 @@ CREATE TABLE 'client' (
 CREATE TABLE 'room' (
     'roomId' int(100) NOT NULL AUTO_INCREMENT,
     'description' LONGTEXT NOT NULL, 
-    'bed_type' enum ('single_bed', 'double_bed', 'king_bed', 'twin_bed') NOT NULL
+    'bed_type' enum ('single_bed', 'double_bed', 'king_bed', 'twin_bed') NOT NULL,
     'adults_number' int(3) NOT NULL,
     'children_number' int(3)  DEFAULT 0,
     'is_reserved' tinyint(4) NOT NULL DEFAULT 0,
     'price_per_night' int(5) NOT NULL,
- PRIMARY KEY ('roomId'),
+    PRIMARY KEY ('roomId'),
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 
 CREATE TABLE 'reservation' (
-'reservationId' int(200),
-'checkIn_Date' datetime NOT NULL,
-'checkOut_Date' datetime NOT NULL,
-'is_active' tinyint(4) NOT NULL,
-'client_code' int(10) NOT NULL,
- PRIMARY KEY ('reservationId'),
- CONSTRAINT `FK_ClientCode` FOREIGN KEY (`client_code`) REFERENCES `client` (`clientId`) ON DELETE NO ACTION ON UPDATE NO ACTION
+    'reservationId' int(200),
+    'checkIn_Date' datetime NOT NULL,
+    'checkOut_Date' datetime NOT NULL,
+    'is_active' tinyint(4) NOT NULL,
+    'client_code' int(10) NOT NULL,
+    PRIMARY KEY ('reservationId'),
+    CONSTRAINT `FK_ClientCode` FOREIGN KEY (`client_code`) REFERENCES `client` (`clientId`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 
 CREATE TABLE 'reservations_inventory' (
